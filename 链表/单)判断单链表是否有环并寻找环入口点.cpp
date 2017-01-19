82a9af199c354efb036ca0ca4adbd28fe0e9acf7
starryskywx <327466897@qq.com>
2017-01-19 16:42:51 +0800

    判断一个单链表是否有环,如果有，找到环的入口点

--- 链表/单)判断单链表是否有环并寻找环入口点.cpp
+++ 链表/单)判断单链表是否有环并寻找环入口点.cpp
@@ -1,15 +1,26 @@
+/**
+ * 判断一个单链表是否有环,如果有，找到环的入口点
+ */
 node* is_looplist(node* head){
 	node *p1, *p2;
 	p1 = p2 = head;
 	if(head == NULL || head->next == NULL){
 		return head;
 	}
+	//判断环是否存在
 	while(p2->next && p2->next->next){
 		p1 = p1->next;
 		p2 = p2->next->next;
 		if(p1==p2){
-			return 1;
+			break;
 		}
 	}
-	return 0;
+	if(p1!=p2) return NULL;
+
+	p1 = head;
+	while(p1 != p2){
+		p1 = p1->next;
+		p2 = p2->next;
+	}
+	return p1;
 }
