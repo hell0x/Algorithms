eb9b832394cfd04570cac9921ea8223c88251bdd
starryskywx <327466897@qq.com>
2017-02-07 18:25:47 +0800

    栈

--- 栈/栈的应用-四则运算求值/栈-四则运算求值.cpp
+++ 栈/栈的应用-四则运算求值/栈-四则运算求值.cpp
@@ -1,6 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stk.h>
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+/*链式栈的实现*/
+typedef int Status;
+typedef int SElemType;
+
+typedef struct StackNode{
+	SElemType data;
+	struct StackNode *next;
+}StackNode, *LinkStackPtr;
+
+typedef struct LinkStack{
+	LinkStackPtr top;
+	int count;
+}LinkStack;
+
+//栈的初始化
+Status init(LinkStack *S){
+	S->top = (LinkStackPtr)malloc(sizeof(StackNode));
+
+	if(NULL == S->top)
+		return ERROR;
+	else{
+		S->top->next = NULL;
+	}
+	return OK;
+}
+
+//是否为空
+Status StackEmpty(LinkStack *S){
+	if(S->top == NULL)
+		return TRUE;
+	else
+		return FALSE;
+}
+
+//入栈
+Status Push(LinkStack *S, SElemType e){
+	LinkStackPtr s = (LinkStackPtr)malloc(sizeof(StackNode));
+	s->data = e;
+	s->next = S->top;
+	S->top = s;
+	S->count++;
+	return OK;
+}
+
+//出栈
+Status Pop(LinkStack *S, SElemType *e){
+	LinkStackPtr p;
+	if(StackEmpty(S))
+		return ERROR;
+	*e = S->top->data;
+	p = S->top;
+	S->top = S->top->next;
+	free(p);
+	S->count--;
+	return OK;
+}
+
+//遍历
+Status traverse(LinkStack *S){
+	LinkStackPtr p = S->top;
+	if(StackEmpty(S))
+		return OK;
+	printf("Elements:");
+	while(p->next){
+		printf("%d ", p->data);
+		p = p->next;
+	}
+	printf("\n");
+	return OK;
+}
+
+//清空栈
+Status clearStack(LinkStack *S){
+	if(StackEmpty(S))
+		return OK;
+	else{
+		LinkStackPtr p = S->top, q;
+		while(p->next){
+			q = p;
+			p = p->next;
+			free(q);
+		}
+		S->top = NULL;
+	}
+	return OK;
+}
+/*链式栈的实现*/
 
 /**
  * isNumber 
@@ -46,7 +138,8 @@ int isRBracket(char c){
  * Priority
  * 对传进来的元素划分优先级
  */
-int Priority(char c){
+int priority(char c){
+	int ret = 0;
 	if(c == '+' || c == '-')
 		ret = 1;
 	else if(c == '*' || c == '/')
@@ -54,4 +147,22 @@ int Priority(char c){
 	else
 		ret = 3;
 	return ret;
+}
+
+
+int main(void){
+	LinkStack S;
+	SElemType e;
+	init(&S);
+	for(int n=0; n<10; n++){
+		Push(&S, n+1);
+	}
+	traverse(&S);
+	Pop(&S, &e);
+	printf("%d\n", e);
+	traverse(&S);
+	clearStack(&S);
+	traverse(&S);
+
+	return 0;
 }
