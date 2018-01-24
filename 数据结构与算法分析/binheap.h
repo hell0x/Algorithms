e04f6fd47ec8ca01f041c0bee95075aae9dd661f
hell0x <327466897@qq.com>
2018-01-24 16:37:36 +0800

    .

---
+++ 数据结构与算法分析/binheap.h
@@ -0,0 +1,20 @@
+		typedef int ElementType;
+
+/*Start*/
+		#ifndef _BinHeap_H
+		#define _BinHeap_H
+
+		struct HeapStruct;
+		typedef struct HeapStruct *PriorityQueue;
+
+		PriorityQueue Initialize(int MaxElements);
+		void Destroy(PriorityQueue H);
+		void MakeEmpty(PriorityQueue H);
+		void Insert(ElementType X, PriorityQueue H);
+		ElementType DeleteMin(PriorityQueue H);
+		ElementType FindMin(PriorityQueue H);
+		int IsEmpty(PriorityQueue H);
+		int IsFull(PriorityQueue H);
+
+		#endif
+/*End*/
